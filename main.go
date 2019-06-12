beddea935c2fd615d0cad6195ce280c31bdbf756
LorenzHW <lorenzhofmann.w@gmail.com>
2019-06-12 13:16:25 +0200

    Commenting not so clean code

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ func main() {
 	inputPath := os.Args[1:][1]
 
 	generatorType := ""
+	// This is not done very well: Trying to figure
+	// out which generator to run based on the input
+	// but things will change anyway probably. So let's
+	// keep it dirty for the moment.
 	if strings.Contains(inputPath, ".descr") {
 		generatorType = "proto-generator"
 	} else if strings.Contains(inputPath, ".pb") {
