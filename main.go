b416ca16bfd2e0b58499b6f08e9356e9e00147b8
LorenzHW <lorenzhofmann.w@gmail.com>
2019-06-21 19:14:30 +0200

    Adding a bit more explanation, so I know what I tried.

--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 	// out which generator to run based on the input
 	// but things will change anyway probably. So let's
 	// keep it dirty for the moment.
+	// However, using flags is not possible, because both generators
+	// parse their own flags, and it is only possible to parse flags once.
 	if strings.Contains(inputPath, ".descr") {
 		generatorType = "proto-generator"
 	} else if strings.Contains(inputPath, ".pb") {
