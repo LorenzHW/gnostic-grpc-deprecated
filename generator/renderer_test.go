3d23a9cb68c4d7d57bf71f28c5010752a18aa458
LorenzHW <lorenzhofmann.w@gmail.com>
2019-06-25 12:36:26 +0200

    Updating tests for merged generator

--- generator/renderer_test.go
+++ generator/renderer_test.go
@@ -1,4 +1,4 @@
-package descriptor_generator
+package generator
 
 import (
 	"github.com/golang/protobuf/proto"
@@ -7,19 +7,29 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"path/filepath"
 	"testing"
+)
+
+const (
+	// When false, test behaves normally, checking output against golden test files.
+	// But when changed to true, running test will actually re-generate golden test
+	// files (which assumes output is correct).
+	regenerateMode = false
+
+	testFilesDirectory = "testfiles"
 )
 
 func TestFileDescriptorGeneratorParameters(t *testing.T) {
 	input := "testfiles/parameters/test.pb"
-	output := "../protoc-generator/testfiles/input/parameter.descr"
 
-	fileDescriptorData, err := runDescriptorGeneratorWithoutEnv(input)
+	protoData, err := runGeneratorWithoutEnvironment(input)
 	if err != nil {
 		t.Errorf("Error while executing the descriptor generator")
 		t.Errorf(err.Error())
 	}
-	writeFile(output, fileDescriptorData)
+
+	checkContents(t, string(protoData), "goldstandard/parameter.proto")
 
 	erroneousInput := []string{"testfiles/parameters/errors/invalid_path_param.pb", "testfiles/parameters/errors/invalid_query_param.pb"}
 
@@ -28,8 +38,9 @@ func TestFileDescriptorGeneratorParameters(t *testing.T) {
 			"The path parameter with the Name param1 is invalid. The path template may refer to one or more fields in the gRPC request message, as long as each field is a non-repeated field with a primitive (non-message) type": true,
 			"The query parameter with the Name param1 is invalid. Note that fields which are mapped to URL query parameters must have a primitive type or a repeated primitive type or a non-repeated message type.":               true,
 		}
-		fileDescriptorData, err = runDescriptorGeneratorWithoutEnv(errorInput)
+		protoData, err = runGeneratorWithoutEnvironment(errorInput)
 		if _, ok := errorMessages[err.Error()]; !ok {
+			// If we don't get an error from the generator the test fails!
 			t.Errorf("Error while executing the descriptor generator")
 			t.Errorf(err.Error())
 		}
@@ -39,28 +50,39 @@ func TestFileDescriptorGeneratorParameters(t *testing.T) {
 
 func TestFileDescriptorGeneratorRequestBodies(t *testing.T) {
 	input := "testfiles/requestBodies/test.pb"
-	output := "../protoc-generator/testfiles/input/requestbodies.descr"
 
-	fileDescriptorData, err := runDescriptorGeneratorWithoutEnv(input)
+	protoData, err := runGeneratorWithoutEnvironment(input)
 	if err != nil {
 		t.Errorf("Error while executing the protoc-generator")
 		t.Errorf(err.Error())
 	}
-	writeFile(output, fileDescriptorData)
+
+	checkContents(t, string(protoData), "goldstandard/requestbodies.proto")
 
 }
 
 func TestFileDescriptorGeneratorResponses(t *testing.T) {
 	input := "testfiles/responses/test.pb"
-	output := "../protoc-generator/testfiles/input/responses.descr"
 
-	fileDescriptorData, err := runDescriptorGeneratorWithoutEnv(input)
+	protoData, err := runGeneratorWithoutEnvironment(input)
 	if err != nil {
 		t.Errorf("Error while executing the protoc-generator")
 		t.Errorf(err.Error())
 	}
-	writeFile(output, fileDescriptorData)
+	checkContents(t, string(protoData), "goldstandard/responses.proto")
+}
 
+func runGeneratorWithoutEnvironment(input string) ([]byte, error) {
+	surfaceModel := buildSurfaceModel(input)
+	r, _ := NewDescriptorRenderer(surfaceModel)
+	r.Package = "testPackage"
+
+	fdSet, err := r.RunFileDescriptorSetGenerator()
+	if err != nil {
+		return nil, err
+	}
+	b, err := r.RenderProto(fdSet)
+	return b, err
 }
 
 func buildSurfaceModel(input string) *surface.Model {
@@ -71,20 +93,29 @@ func buildSurfaceModel(input string) *surface.Model {
 	return surfaceModel
 }
 
-func runDescriptorGeneratorWithoutEnv(input string) ([]byte, error) {
-	packageName := "testPackage"
-
-	surfaceModel := buildSurfaceModel(input)
-	descriptorRenderer, _ := NewDescriptorRenderer(surfaceModel)
-	descriptorRenderer.Package = packageName
-	fileDescriptorSetData, err := descriptorRenderer.BuildFileDescriptorSet()
-	return fileDescriptorSetData, err
-}
-
 func writeFile(output string, protoData []byte) {
 	dir := path.Dir(output)
 	os.MkdirAll(dir, 0755)
 	f, _ := os.Create(output)
 	defer f.Close()
 	f.Write(protoData)
+}
+
+func checkContents(t *testing.T, actualContents string, goldenFileName string) {
+	goldenFileName = filepath.Join(testFilesDirectory, goldenFileName)
+
+	if regenerateMode {
+		writeFile(goldenFileName, []byte(actualContents))
+	}
+
+	// verify that output matches golden test files
+	b, err := ioutil.ReadFile(goldenFileName)
+	if err != nil {
+		t.Errorf("Error while reading goldstandard file")
+		t.Errorf(err.Error())
+	}
+	goldstandard := string(b)
+	if goldstandard != actualContents {
+		t.Errorf("File contents does not match.")
+	}
 }
